e0f3c56958a446d0d325c8873596772918697a56
Samuel Ryan <sam@samryan.co.uk>
2020-08-05 21:24:13 +0100

    :bug: Add tag specific behaviour to support releases (#2)

--- dist/index.js
+++ dist/index.js
@@ -4219,57 +4219,73 @@ const github = __webpack_require__(469);
 const fs = __webpack_require__(747);
 const tmpl = __webpack_require__(216);
 const openpgp = __webpack_require__(724);
+const path = __webpack_require__(622);
 
 async function run() {
   try {
-    const template = fs.readFileSync('templates/' + core.getInput('template'), 'utf8');
-    const destination = core.getInput('destination');
-    const primary = core.getInput('primary');
-    const secondary = core.getInput('secondary');
-
     const { context } = github;
     const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
-
     const { owner, repo } = context.repo;
-    const commitSha = context.payload.after;
-    const { ref } = context;
 
-    let label = commitSha;
+    let label = null;
+    let verification = null;
+    let author = null;
 
-    const commit = await octokit.git.getCommit({ owner, repo, commit_sha: commitSha });
+    const { data: reference } = await octokit.request('GET /repos/:owner/:repo/git/refs/:ref', {
+      repo,
+      owner,
+      ref: context.ref.replace('refs/', ''),
+    });
 
-    if (/^refs\/tags\//.test(ref)) {
-      label = ref.replace('refs/tags/', '');
+    if (reference.object.type === 'tag') {
+      const { data: tag } = await octokit.git.getTag({
+        owner,
+        repo,
+        tag_sha: reference.object.sha,
+      });
+
+      label = reference.ref.replace('refs/tags/', '');
+      author = tag.tagger;
+      verification = tag.verification;
     }
 
-    if (commit.data.verification.verified === false) {
-      throw new Error('Commit must be signed and verified with GitHub.');
+    if (reference.object.type === 'commit') {
+      const { data: commit } = await octokit.git.getCommit({
+        owner,
+        repo,
+        commit_sha: reference.object.sha,
+      });
+
+      label = reference.object.sha;
+      author = commit.author;
+      verification = commit.verification;
     }
 
-    const { signature } = commit.data.verification;
+    if (verification == null || verification.verified === false) {
+      throw new Error('Object (commit, tag) must be signed and verified with GitHub.');
+    }
 
     const key = (
-      await openpgp.message.readArmored(signature)
+      await openpgp.message.readArmored(verification.signature)
     )
       .getSigningKeyIds()[0]
       .toHex()
       .toUpperCase();
 
+    const destination = core.getInput('destination');
+    const primary = core.getInput('primary');
+    const secondary = core.getInput('secondary');
+    const template = fs.readFileSync(
+      `${path.dirname(eval('__filename'))}/../templates/${core.getInput('template')}`,
+      'utf8',
+    );
+
+    const url = `https://github.com/${owner}/${repo}/tree/${label}`;
+
     const data = {
-      theme: {
-        primary,
-        secondary,
-      },
-      commit: {
-        url: commit.data.parents[0].html_url,
-        label,
-        at: commit.data.author.date,
-        key,
-      },
-      author: {
-        name: commit.data.author.name,
-        email: commit.data.author.email,
-      },
+      theme: { primary, secondary },
+      object: { url, label, key },
+      author,
     };
 
     fs.writeFileSync(destination, tmpl(template, data));
